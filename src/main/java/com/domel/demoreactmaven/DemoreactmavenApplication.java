9f9ec47f3aeb3eed43d0d6ddf0d07eb37b8976c2
Tomasz Prokop <tprokop@csod.com>
2021-04-06 09:02:29 +0200

    frontend

---
+++ src/main/java/com/domel/demoreactmaven/DemoreactmavenApplication.java
@@ -0,0 +1,13 @@
+package com.domel.demoreactmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoreactmavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoreactmavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/domel/demoreactmaven/DemoreactmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domel.demoreactmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoreactmavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
